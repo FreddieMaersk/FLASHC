c61adfc5eff3bc5c448a4b30f8de2c68c7d01589
Fredd <165827723+FreddieMaersk@users.noreply.github.com>
2024-09-24 17:10:11 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos EUA?',
+    'A capital dos EUA é Washington DC'
+)
+
+criaCartao(
+    'Música',
+    'Quem canta a música "I Want To Break Free"?',
+    'Freddie Mercury da banda Queen'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Árvore em Inglês?',
+    'Oi em ingles é Tree (tri)'
+)
+criaCartao(
+    'Português',
+        'Como se diz oi de forma informal?',
+        'Oi de forma informal aqui no Brasil é "falaê'
+    )
+    criaCartao(
+        'Lingua francesa',
+        'Como se diz oi em Francês?',
+        'Oi em frances é HÉ'
+    )
